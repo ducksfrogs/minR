245d43d5c4f3e77dab3915252dd4c1f5ce47676d
ma <ma_yamaki@yahoo.com>
2020-06-28 13:05:39 +0900

    dd

--- tidy/nycflight.R
+++ tidy/nycflight.R
@@ -10,3 +10,18 @@ jan1 <- filter(flights, month==1, day==1)
 filter(flights, month==11|month==12)
 nov_dec <- filter(flights, month==11|month==12)
 filter(flights, !(arr_delay >120 | dep_delay >120))
+
+arrange(flights, year, month, day)
+flights
+arrange(flights, desc(dep_delay))
+
+df <- tibble(x=c(5,2, NA))
+arrange(df, x)
+arrange(df, desc(x))
+select(flights, year, month, day)
+select(flights, year:day)
+select(flights, -(year:day))
+rename(flights, tail_num = tainum)
+select(flights, time_hour, air_time, everything())
+diamonds %>%
+  count(cut)
