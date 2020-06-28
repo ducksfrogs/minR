04e441faeaa579554af2854b9adaf1ec7be28e5a
ma <ma_yamaki@yahoo.com>
2020-06-28 11:58:52 +0900

    dddddd

---
+++ second.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+diamonds
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x=cut))
+
+demo <- tribble(
+  ~cut, ~freq,
+  "Fair", 1610,
+  "Good", 4906,
+  "Very Good", 12082,
+  "Premium", 13791,
+  "Ideal", 21551
+)
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x=cut, y=freq), stat = "identity")
+library(tribble)
